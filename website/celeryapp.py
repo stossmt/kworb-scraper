8beb5665324c3b3fad21fd1933920b16ca223e3f
Matt Stoss <mts74@pitt.edu>
2018-06-16 20:56:38 -0400

    Configure celery

--- website/celeryapp.py
+++ website/celeryapp.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
+from website import celeryconfig
 
 # This document serves as the entrance point to celery
 
