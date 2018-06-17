4d11177d30713a5621f8230470a5626da1c835ed
Matt Stoss <mts74@pitt.edu>
2018-06-16 20:54:12 -0400

    Configure celery

--- website/celeryapp.py
+++ website/celeryapp.py
@@ -16,4 +16,4 @@ app.autodiscover_tasks([])
 # Dumps its own request information
 @app.task(bind=True)
 def debug_task(self):
-print('Request: {0!r}'.format(self.request))
+    print('Request: {0!r}'.format(self.request))
