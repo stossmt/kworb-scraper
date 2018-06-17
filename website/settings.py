0101159b79d47127c74373f7a0d6070fe3cf4215
Matt Stoss <mts74@pitt.edu>
2018-06-16 20:27:44 -0400

    Environment config

--- website/settings.py
+++ website/settings.py
@@ -5,7 +5,7 @@ import json
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'kworb-scraper-staging.herokuapp.com',]
 
 DEBUG = os.environ.get('DEBUG')
 
