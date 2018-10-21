f88caeef4f67f789b08f751fa4999f42c3dc61fe
Matt Stoss <mts74@pitt.edu>
2018-10-20 22:03:09 -0400

    Heroku static files correction

--- website/settings.py
+++ website/settings.py
@@ -82,6 +82,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
