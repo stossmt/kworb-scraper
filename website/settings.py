3949b8122939aa6a5fcc04fc05197270da9a927f
Matt Stoss <mts74@pitt.edu>
2018-10-20 22:26:11 -0400

    heroku correction

--- website/settings.py
+++ website/settings.py
@@ -3,7 +3,7 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-DEBUG = os.environ.get('DEBUG')
+DEBUG = os.environ.get('debug')
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
