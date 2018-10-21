d891bc4f5c90ccc3d32be84959d0a1660af552f4
Matt Stoss <mts74@pitt.edu>
2018-10-20 22:27:57 -0400

    heroku correction

--- website/settings.py
+++ website/settings.py
@@ -1,4 +1,5 @@
 import os
+import django_heroku
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -111,5 +112,4 @@ if not DEBUG:
         }
     }
 
-    import django_heroku
     django_heroku.settings(locals())
