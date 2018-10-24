59c200f9093cbe219b3209bbaa563698d20b0d89
Matt Stoss <mts74@pitt.edu>
2018-10-23 23:42:02 -0400

    Heroku debugging

--- website/settings.py
+++ website/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 ALLOWED_HOSTS = ['127.0.0.1', 'kworb-scraper-staging.herokuapp.com', 'kworb-scraper.herokuapp.com']
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 INSTALLED_APPS = [
     'django.contrib.admin',
