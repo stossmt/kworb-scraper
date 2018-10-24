c7c35da4a89423e7423c1b5b6ce9e4f5f53e3507
Matt Stoss <mts74@pitt.edu>
2018-10-23 23:18:17 -0400

    Heroku debug

--- website/settings.py
+++ website/settings.py
@@ -84,6 +84,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 '''
 if DEBUG:
     DATABASES = {
