4efb6a8f0db170e49ebc64e3945ed21aca9335ef
Matt Stoss <mts74@pitt.edu>
2018-10-23 22:28:32 -0400

    Heroku debug

--- website/settings.py
+++ website/settings.py
@@ -16,7 +16,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    # 'django.contrib.staticfiles',
+    'django.contrib.staticfiles',
     'celerybeat_status',
     'kworb.apps.KworbConfig',
     'dashboard.apps.DashboardConfig',
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
