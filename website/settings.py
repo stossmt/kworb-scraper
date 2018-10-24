04e5605fb025d66b4e7da36a2ec8fddb8c68aa59
Matt Stoss <mts74@pitt.edu>
2018-10-23 22:08:25 -0400

    Heroku debug

--- website/settings.py
+++ website/settings.py
@@ -101,20 +101,4 @@ if DEBUG:
 if not DEBUG:
 '''
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['console'],
-             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
-        },
-    },
-}
-
 django_heroku.settings(locals())
