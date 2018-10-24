65cb406787b0a0d85eba5f337c3d18bc79342c64
Matt Stoss <mts74@pitt.edu>
2018-10-23 23:22:34 -0400

    Heroku debugging

--- website/settings.py
+++ website/settings.py
@@ -83,7 +83,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
-
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static/css'),
     os.path.join(BASE_DIR, 'static/images'),
--- website/wsgi.py
+++ website/wsgi.py
@@ -1,7 +1,7 @@
 import os
-
-from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "website.settings")
 
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
