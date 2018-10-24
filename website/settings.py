6e05f8b4538912c5992b63e631eb9904e139866a
Matt Stoss <mts74@pitt.edu>
2018-10-23 20:44:34 -0400

    Heroku debugging

--- website/settings.py
+++ website/settings.py
@@ -4,7 +4,7 @@ import django_heroku
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-DEBUG = False
+DEBUG = os.environ.get('DEBUG')
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
