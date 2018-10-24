99796cfd7dcd38add1876fb563bff9d3d95db889
Matt Stoss <mts74@pitt.edu>
2018-10-23 21:43:37 -0400

    Heroku debug

--- website/settings.py
+++ website/settings.py
@@ -80,15 +80,15 @@ USE_L10N = True
 
 USE_TZ = True
 
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 if DEBUG:
-    STATIC_URL = '/static/'
-
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static'),
-    ]
-
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
