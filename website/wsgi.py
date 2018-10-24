b332283d0aea02d018dc07dd7203b9617f9ad38e
Matt Stoss <mts74@pitt.edu>
2018-10-23 23:26:50 -0400

    Heroku debugging

--- website/wsgi.py
+++ website/wsgi.py
@@ -1,7 +1,7 @@
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "website.settings")
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "website.settings")
+
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
